a27839a768e35e365a253ce783bd141d89ebfd72
Kingkonbro <konradboruch735@gmail.com>
2025-11-23 21:03:18 +0000

    Completed - add comments

---
+++ Challenge_4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Challenge_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] preformances = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                bool validNumber = false;
+                while (!validNumber)
+                {
+                    Console.Write("Number of tickets sold: ");
+                    int ticketsSold = Convert.ToInt32(Console.ReadLine());
+                    if (rangeCheck(ticketsSold, 0, 120))
+                    {
+                        validNumber = true;
+                        preformances[i] = ticketsSold;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid Number");
+                    }
+                }
+            }
+            int total = 0;
+            foreach (int sold in preformances)
+            {
+                total += sold;
+            }
+            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Average: {total/4}");
+        }
+        static bool rangeCheck(int value, int max, int min)
+        {
+            bool valid = false;
+            if ( (value <= min) && (value >= max) )
+            {
+                valid = true;
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ Challenge_5/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            enterAttributes();
+        }
+        static int getNumber()
+        {
+            int number = 0;
+            bool valid = false;
+            while (!valid)
+            {
+                Console.Write("Enter number between 0 and 100: ");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                if ((number >= 0) && (number <= 100))
+                {
+                    valid = true;
+                }
+
+                if (!valid)
+                {
+                    Console.WriteLine("That number isn't between 0 and 100, please enter again.");
+                }
+
+                
+            }
+            return number;
+        }
+        static string getName()
+        {
+            bool valid = false;
+            string name = "";
+            while (!valid)
+            {
+                Console.Write("Enter Name: ");
+                name = Console.ReadLine();
+
+                if ((name.Length) > 0)
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Name cannot be left blank.");
+                }
+            }
+            return name;
+        
+        }
+        static void enterAttributes()
+        {
+            int number0 = getNumber();
+            int number1 = getNumber();
+            string name = getName();
+            saveChanges(name, number0, number1);
+        }
+        static void saveChanges(string name, int number0, int number1)
+        {
+            try
+            {
+                string filePath = (Directory.GetCurrentDirectory()) + @"/attributes.csv";
+                StreamWriter file = new StreamWriter(filePath, true);
+                file.WriteLine($"{name},{number0},{number1}");
+                file.Close();
+            }
+            catch
+            {
+                Console.WriteLine("The file couldn't be written to.");
+            }
+        }
+    }
+}
